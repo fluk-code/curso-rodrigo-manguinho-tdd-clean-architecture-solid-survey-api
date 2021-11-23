cc7722a4809664e0fa565e2ce99ce055c11ce270
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-23 10:57:48 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,18 @@
+import { IAddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { IAccountModel } from '../../../../domain/models/account'
+import { IAddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements IAddAccountRepository {
+  async add (accountData: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getConnetion('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const id = String(result.insertedId)
+    const account = {
+      ...accountData,
+      id
+    }
+
+    return account
+  }
+}
