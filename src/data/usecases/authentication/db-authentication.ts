688df11a764112d526a0500dde11cd985954d58d
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-25 11:41:12 -0300

    feat: ensure DbAthentication returns a token on success

--- src/data/usecases/authentication/db-authentication.ts
+++ src/data/usecases/authentication/db-authentication.ts
@@ -21,8 +21,11 @@ export class DBAuthentication implements IAuthentication {
   async auth (authentication: IAuthenticationModel): Promise<string> {
     const account = await this.loadAccountByEmailRepository.load(authentication.email)
     if (account) {
-      await this.hashComparer.compare(authentication.password, account.password)
-      await this.tokenGenerator.generate(account.id)
+      const isPasswordValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isPasswordValid) {
+        const accessToken = await this.tokenGenerator.generate(account.id)
+        return accessToken
+      }
     }
 
     return null as unknown as string
