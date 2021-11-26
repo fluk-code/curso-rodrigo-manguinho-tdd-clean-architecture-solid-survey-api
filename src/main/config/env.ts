61767b51ca38d9e0a6a70f7e11ff544aa6c7889e
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-26 09:36:14 -0300

    refactor: add login factory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
   mongoURL: process.env.MONGO_URL ?? 'mongodb://root:survey@localhost:27017/clean-node-api?authSource=admin',
-  port: process.env.PORT ?? 5050
+  port: process.env.PORT ?? 5050,
+  jwtSecret: process.env.JWT_SECRET ?? 'ekf-2919'
 }
