d25e116817bb283620a0c4b41603be708fbc1e9a
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-29 13:51:18 -0300

    feat: add AddSruvey composition

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.spec.ts
@@ -0,0 +1,20 @@
+
+import { IValidation } from '../../../../presentation/protocols/valitation'
+import { makeAddSurveyValidation } from './add-survey-validation-factory'
+import { RequiredFieldValidation, ValidationComposite } from '../../../../validation/validators'
+
+jest.mock('../../../../validation/validators/validation-composite')
+
+describe('AddSurveyValidation Factory', () => {
+  it('Should call ValidationComposite with all validations', () => {
+    makeAddSurveyValidation()
+
+    const validations: IValidation[] = []
+
+    for (const field of ['question', 'answers']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
