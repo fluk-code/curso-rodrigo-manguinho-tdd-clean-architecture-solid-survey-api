a787a31b474a3705992d2d6197411f1e70386046
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-30 14:58:32 -0300

    feat: ensure load surveys returns 403 if no access token is provided

---
+++ src/main/factories/controllers/survey/load-surveys/load-surveys-controller-factory.ts
@@ -0,0 +1,10 @@
+
+import { LoadSurveysController } from '../../../../../presentation/controllers/survey/load-surveys/load-surveys-controller'
+import { IController } from '../../../../../presentation/protocols'
+import { makeLogControllerDecorator } from '../../../decorators/log-controller-decorator-factory'
+import { makeDbLoadSurveys } from '../../../usecases/survey/load-surveys/db-load-surveys'
+
+export const makeLoadSurveyController = (): IController => {
+  const controller = new LoadSurveysController(makeDbLoadSurveys())
+  return makeLogControllerDecorator(controller)
+}
