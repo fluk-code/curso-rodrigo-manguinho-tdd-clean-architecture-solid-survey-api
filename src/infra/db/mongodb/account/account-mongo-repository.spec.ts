7c05099ef2689889d9baae285965a9c025b944aa
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-29 23:57:18 -0300

    test: ensure AccountMongoRepository will not find an account if find account by token fails

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -116,4 +116,11 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
   })
+
+  it('Should return null if loadByToken fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByToken('invalid_token')
+
+    expect(account).toBeFalsy()
+  })
 })
