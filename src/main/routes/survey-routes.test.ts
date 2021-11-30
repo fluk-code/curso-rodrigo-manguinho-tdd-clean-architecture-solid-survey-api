43bd51208f1950350ecd4c7f07decc3b15ea4548
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-30 14:58:49 -0300

    test: ensure load surveys returns 403 if no access token is provided

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -72,4 +72,12 @@ describe('Sorvey Routes', () => {
         .expect(204)
     })
   })
+
+  describe('GET /surveys', () => {
+    it('Should return 403 on load survey without accessToken', async () => {
+      await request(app)
+        .get('/api/surveys')
+        .expect(403)
+    })
+  })
 })
