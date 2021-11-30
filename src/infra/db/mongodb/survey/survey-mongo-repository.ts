4a40102be2aab5faf3e801ee0c00762a309fa141
Felipe Ferreira <ferreirafelipericardo@gmail.com>
2021-11-30 14:47:15 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,10 +1,18 @@
 
+import { ILoadSurveysRepository } from '../../../../data/protocols/db/survey/load-surveys-repository'
 import { IAddSurveyModel, IAddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { ISurveyModel } from '../../../../domain/models/survey'
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class SurveyMongoRepository implements IAddSurveyRepository {
+export class SurveyMongoRepository implements IAddSurveyRepository, ILoadSurveysRepository {
   async add (surveyData: IAddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<ISurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray()
+    return surveys as unknown as ISurveyModel[]
   }
 }
